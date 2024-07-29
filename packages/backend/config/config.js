df683afce72eb17bbdeb6a90f14d19886eebc4b4
rickyluo <hualazimo@live.cn>
2024-07-29 15:56:45 +0800

    config: update docker config

--- packages/backend/config/config.js
+++ packages/backend/config/config.js
@@ -5,11 +5,11 @@ module.exports = {
     host: "database", //开发模式下使用localhost, 生产模式下使用database容器名称
     port: 3306,
     user: "mysql",
-    password: "mysql",
+    password: "gaga!1234",
   },
   security: {
     secretKey: "secretKey",
-    // 过期时间 1小时
-    expiresIn: 60 * 60,
+    // 过期时间 6小时
+    expiresIn: 60 * 60 * 6,
   },
 };
